a103cfb8c8d3ce502868f09d371740bd5e78ff9f
Jok <58263217+Jok98@users.noreply.github.com>
2022-05-29 17:22:02 +0200

    aggiunti commenti classe registrazione

--- src/cittadini/Registrazione.java
+++ src/cittadini/Registrazione.java
@@ -62,69 +62,86 @@ public class Registrazione {
 		frame.getContentPane().setLayout(null);
 		
 
-		
+		//start lblNome
 		JLabel lblNome = new JLabel("Nome :");
 		lblNome.setBounds(10, 11, 112, 14);
 		frame.getContentPane().add(lblNome);
 		
+		//start lblCognome
 		JLabel lblCognome = new JLabel("Cognome :");
 		lblCognome.setBounds(10, 42, 112, 14);
 		frame.getContentPane().add(lblCognome);
 		
+		//start tfNome
 		tfNome = new JTextField();
 		tfNome.setBounds(174, 8, 250, 20);
 		frame.getContentPane().add(tfNome);
 		tfNome.setColumns(10);
 		
+		//start tfCognome
 		tfCognome = new JTextField();
 		tfCognome.setBounds(174, 39, 250, 20);
 		frame.getContentPane().add(tfCognome);
 		tfCognome.setColumns(10);
 		
+		//start tfCodiceFiscale
 		tfCodiceFiscale = new JTextField();
 		tfCodiceFiscale.setBounds(174, 70, 250, 20);
 		frame.getContentPane().add(tfCodiceFiscale);
 		tfCodiceFiscale.setColumns(10);
 		
+		//start lblCodicefiscale
 		JLabel lblCodicefiscale = new JLabel("Codice Fiscale :");
 		lblCodicefiscale.setBounds(10, 73, 112, 14);
 		frame.getContentPane().add(lblCodicefiscale);
 		
+		//start tfEmail
 		tfEmail = new JTextField();
 		tfEmail.setBounds(174, 101, 250, 20);
 		frame.getContentPane().add(tfEmail);
 		tfEmail.setColumns(10);
 		
+		//start lblEmail
 		JLabel lblEmail = new JLabel("Email :");
 		lblEmail.setBounds(10, 104, 112, 14);
 		frame.getContentPane().add(lblEmail);
 		
+		//start tfUserID
 		tfUserID = new JTextField();
 		tfUserID.setBounds(174, 132, 250, 20);
 		frame.getContentPane().add(tfUserID);
 		tfUserID.setColumns(10);
 		
+		//start lblUserID
 		JLabel lblUserID = new JLabel("User ID");
 		lblUserID.setBounds(10, 135, 46, 14);
 		frame.getContentPane().add(lblUserID);
 		
+		//start lblPassword
 		JLabel lblPassword = new JLabel("Password :");
 		lblPassword.setBounds(10, 166, 112, 14);
 		frame.getContentPane().add(lblPassword);
 		
+		//start passwordField
 		passwordField = new JPasswordField();
 		passwordField.setBounds(174, 163, 250, 20);
 		frame.getContentPane().add(passwordField);
 		
+		//start lblIDUnicoVax
 		JLabel lblIDUnicoVax = new JLabel("Id univoco vaccinazione :");
 		lblIDUnicoVax.setBounds(10, 197, 150, 14);
 		frame.getContentPane().add(lblIDUnicoVax);
 		
+		//start tfIDUnivocoVax
 		tfIDUnivocoVax = new JTextField();
 		tfIDUnivocoVax.setBounds(174, 194, 250, 20);
 		frame.getContentPane().add(tfIDUnivocoVax);
 		tfIDUnivocoVax.setColumns(10);
 		
+		//Start btnRegistra
+		/* l'actionPerformed controlla i dati inseriti dall'utente e 
+		li utilizza per creare una nuova istanza della classe user per inserire così 
+		l'utente nella tabella dei cittadini_registrati presente nel db */
 		JButton btnRegistra = new JButton("Registrati");
 		btnRegistra.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -172,7 +189,9 @@ public class Registrazione {
 		});
 		btnRegistra.setBounds(174, 248, 89, 23);
 		frame.getContentPane().add(btnRegistra);
+		//end btnRegistra
 		
+		//Start btnBack
 		JButton btnBack = new JButton("Indietro");
 		btnBack.addActionListener(new ActionListener() {
 			
@@ -184,13 +203,14 @@ public class Registrazione {
 		});
 		btnBack.setBounds(10, 248, 89, 23);
 		frame.getContentPane().add(btnBack);
+		//end btnBack
 		
-		/**
-		 * 
-		 */
+		
+		//Start lbl_CentroVax
 		JLabel lbl_CentroVax = new JLabel("Centro vaccinale selezionato :");
 		lbl_CentroVax.setBounds(10, 223, 150, 14);
 		frame.getContentPane().add(lbl_CentroVax);
+		//end lbl_CentroVax
 		
 		//Start tf_NomeCentroVax
 		tf_NomeCentroVax = new JTextField();
@@ -211,6 +231,7 @@ public class Registrazione {
 		frame.getContentPane().add(btnPulisci);
 		//end btnPulisci
 	}
+	
 	static void CleanAll() {
 		
 		tfNome.setText("");
